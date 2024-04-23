12b413575d586bdbf2b1500c941e2053a164ad9e
imctlam <45737109+imctlam@users.noreply.github.com>
2024-04-23 09:19:47 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# imctlam.github.io
---
+++ script.js
@@ -0,0 +1,27 @@
+function startExam() {
+    document.getElementById('startContainer').classList.add('hidden');
+    document.getElementById('examContainer').classList.remove('hidden');
+    startCountdown();
+}
+
+function startCountdown() {
+    var countdownElement = document.getElementById('countdown');
+    var countdownTime = 40 * 60; // 40 phút
+    var countdownInterval = setInterval(function() {
+        var minutes = Math.floor(countdownTime / 60);
+        var seconds = countdownTime % 60;
+        countdownElement.innerHTML = `${minutes} phút ${seconds} giây`;
+        countdownTime--;
+        if (countdownTime < 0) {
+            clearInterval(countdownInterval);
+            countdownElement.innerHTML = 'Hết giờ!';
+        }
+    }, 1000);
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
